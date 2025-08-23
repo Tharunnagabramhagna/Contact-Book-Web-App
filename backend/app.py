60e06878a452c3ddac5ff53678879d4c539cd426
tharunnagabramhagna <tharunnagabramhagna@gmail.com>
2025-08-23 17:31:03 +0530

    Change in backend folder

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -10,6 +9,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+import routes
 
 with app.app_context():
     db.create_all()
